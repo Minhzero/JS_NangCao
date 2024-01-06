86c2a7f5f4e34c4533c47ec58fa53390d85e9e7b
Minhzero <minhcongtu12321@gmail.com>
2024-01-06 10:57:34 +0700

    Js_nangcao

---
+++ 01-variables-data-types/03-variables/script.js
@@ -0,0 +1,28 @@
+// Ways to declare a variable
+// `var`, `let`, & `const`
+
+let firstName = 'John';
+const lastName = 'Doe';
+const age = 30;
+
+console.log("age",age);
+
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
+
+const arr = [1, 2, 3, 4];//[giatri1,giatri2]
+arr.push(5);
+console.log(arr);
+
+//Object tenBien={} // OOP
+const person = {
+    name: 'minhnq',
+    hasmoto:'car',
+    age:34,
+  };
+  person.name = 'John';
+  person.email = 'brad@gmail.com';
+  console.log(person);
---
+++ 01-variables-data-types/04-data-types/script.js
@@ -0,0 +1,48 @@
+// String
+const firstName = 'Sara';
+
+// Number
+const age = 30;
+const temp = 98.9;
+
+// Boolean
+const hasKids = true;
+
+// Null
+const aptNumber = null;
+
+//phan biet null vs undefined
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+// BigInt
+const n = 9007199254740991n;
+
+// Reference Types
+
+const numbers = [1, 2, 3, 4];
+
+const person = {
+  name: 'Brad',
+};
+
+function sayHello() {  
+  // ten funcion: bat dau dong tu , +Danh tu bo nghia : getProductList,delete
+  console.log('Xin chào');
+}
+sayHello();
+
+// const output = sayHello;
+
+// console.log(output, typeof output);
+
+// // More info on why typeof null == object
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof#typeof_null
+
+// //  More info on the "function object" type
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
